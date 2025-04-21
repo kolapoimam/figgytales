21dc5cae35af8e799586f34cf2af31e942fabe81
Kolapo Imam <kolapoimam1@gmail.com>
2025-04-21 14:42:06 +0100

    Update App.tsx
    Header Import

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import ShareView from "./pages/ShareView";
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
 import HistoryView from "./pages/HistoryView";
+import Header from "./components/Header";
 
 // Create a client
 const queryClient = new QueryClient();
