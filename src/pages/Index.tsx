72c230e217dba282f84b9dd0a331c341e4621bdf
Kolapo Imam <kolapoimam1@gmail.com>
2025-04-21 14:54:11 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import FileUploader from '@/components/FileUploader';
 import PreviewGrid from '@/components/PreviewGrid';
+import Header from '@/components/Header';
 import SettingsPanel from '@/components/SettingsPanel';
 import RoadmapSection from '@/components/RoadmapSection';
 import Footer from '@/components/Footer';
