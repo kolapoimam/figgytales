e09ef36dfba0b111bb803cd5b8c2bb95320066f3
Kolapo Imam <kolapoimam1@gmail.com>
2025-04-24 17:06:55 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,6 @@ import ShareView from "./pages/ShareView";
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
 import HistoryView from "./pages/HistoryView";
-import Header from "./components/Header";
-
 // Create a client
 const queryClient = new QueryClient();
 
